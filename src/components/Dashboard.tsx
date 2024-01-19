961972fd2b65022e0daee3af615da4a2aa230b3f
YvesStraten <yves.straten@gmail.com>
2024-01-19 09:08:00 +0800

    Chore: format

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -7,63 +7,63 @@ import "./Dashboard.css";
 import { dashboard } from "../types/types";
 
 const Dashboard = ({ tahanData, settingsData }: dashboard) => {
-	/* 	Stores states for the cog, data and settings */
-	const [hover, setHover] = useState<boolean>(false);
+  /* 	Stores states for the cog, data and settings */
+  const [hover, setHover] = useState<boolean>(false);
 
-	return (
-		<>
-			<div className="header">
-				<h1>Data Tahan-Tahan</h1>
-				<Link
-					to="/settings"
-					id="cog"
-					onMouseEnter={() => setHover(true)}
-					onMouseLeave={() => setHover(false)}
-				>
-					<FontAwesomeIcon icon={faCog} className={hover ? "spin" : ""} />
-				</Link>
-			</div>
-			<div className="settings_container">
-				<h1>Current settings: </h1>
-				<div className="settings">
-					{settingsData.map((setting) => {
-						// Maps over the settings
-						const name = setting.setting;
-						if (name.includes("humidity")) {
-							return (
-								<h2 key={setting.setting}>
-									{setting.setting}: {setting.value} %
-								</h2>
-							);
-						} else if (name.includes("lux")) {
-							return (
-								<h2 key={setting.setting}>
-									{setting.setting}: {setting.value} lx
-								</h2>
-							);
-						} else {
-							return (
-								<h2 key={setting.setting}>
-									{setting.setting}: {setting.value} °C
-								</h2>
-							);
-						}
-					})}
-				</div>
-			</div>
-			<div className="main_container">
-				{tahanData.map((tahan) => {
-					if (tahan.name.includes("Humidity")) {
-						return <Display key={tahan.id} char="%" tahan={tahan} />;
-					} else if (tahan.name.includes("Light")) {
-						return <Display key={tahan.id} char="lx" tahan={tahan} />;
-					} else {
-						return <Display key={tahan.id} char="°C" tahan={tahan} />;
-					}
-				})}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="header">
+        <h1>Data Tahan-Tahan</h1>
+        <Link
+          to="/settings"
+          id="cog"
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <FontAwesomeIcon icon={faCog} className={hover ? "spin" : ""} />
+        </Link>
+      </div>
+      <div className="settings_container">
+        <h1>Current settings: </h1>
+        <div className="settings">
+          {settingsData.map((setting) => {
+            // Maps over the settings
+            const name = setting.setting;
+            if (name.includes("humidity")) {
+              return (
+                <h2 key={setting.setting}>
+                  {setting.setting}: {setting.value} %
+                </h2>
+              );
+            } else if (name.includes("lux")) {
+              return (
+                <h2 key={setting.setting}>
+                  {setting.setting}: {setting.value} lx
+                </h2>
+              );
+            } else {
+              return (
+                <h2 key={setting.setting}>
+                  {setting.setting}: {setting.value} °C
+                </h2>
+              );
+            }
+          })}
+        </div>
+      </div>
+      <div className="main_container">
+        {tahanData.map((tahan) => {
+          if (tahan.name.includes("Humidity")) {
+            return <Display key={tahan.id} char="%" tahan={tahan} />;
+          } else if (tahan.name.includes("Light")) {
+            return <Display key={tahan.id} char="lx" tahan={tahan} />;
+          } else {
+            return <Display key={tahan.id} char="°C" tahan={tahan} />;
+          }
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -7,118 +7,118 @@ import { formtype } from "../types/types";
 
 /* Array of all the possible inputs */
 const Fields = [
-	{
-		label: "Max temperature:",
-		type: "number",
-		id: "maxtemp",
-	},
+  {
+    label: "Max temperature:",
+    type: "number",
+    id: "maxtemp",
+  },
 
-	{
-		label: "Min temperature:",
-		type: "number",
-		id: "mintemp",
-	},
+  {
+    label: "Min temperature:",
+    type: "number",
+    id: "mintemp",
+  },
 
-	{
-		label: "Max humidity:",
-		type: "number",
-		id: "maxhumid",
-	},
+  {
+    label: "Max humidity:",
+    type: "number",
+    id: "maxhumid",
+  },
 
-	{
-		label: "Min humidity:",
-		type: "number",
-		id: "minhumid",
-	},
+  {
+    label: "Min humidity:",
+    type: "number",
+    id: "minhumid",
+  },
 
-	{
-		label: "Max Light:",
-		type: "number",
-		id: "maxlight",
-	},
+  {
+    label: "Max Light:",
+    type: "number",
+    id: "maxlight",
+  },
 
-	{
-		label: "Min Light:",
-		type: "number",
-		id: "minlight",
-	},
+  {
+    label: "Min Light:",
+    type: "number",
+    id: "minlight",
+  },
 ];
 
 const Settings = () => {
-	// Track the form's state and handle any change
-	const navigate = useNavigate();
-	const [formData, setFormData] = useState<formtype>({
-		maxtemp: "",
-		mintemp: "",
-		maxhumid: "",
-		minhumid: "",
-		maxlight: "",
-		minlight: "",
-	});
+  // Track the form's state and handle any change
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState<formtype>({
+    maxtemp: "",
+    mintemp: "",
+    maxhumid: "",
+    minhumid: "",
+    maxlight: "",
+    minlight: "",
+  });
 
-	const handleChange = (event: any) => {
-		const { name, value } = event.target;
-		setFormData((prevFormData) => ({
-			...prevFormData,
-			[name]: parseInt(value),
-		}));
-	};
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: parseInt(value),
+    }));
+  };
 
-	const updateSettings = async (event: any) => {
-		/* 		Updates settings by adding them each as a query
+  const updateSettings = async (event: any) => {
+    /* 		Updates settings by adding them each as a query
 		Param to the request */
-		event?.preventDefault();
-		let base = new URL(
-			"https://api.thingspeak.com/update?api_key=KREIMHGC02O4Z5OU",
-		);
+    event?.preventDefault();
+    let base = new URL(
+      "https://api.thingspeak.com/update?api_key=KREIMHGC02O4Z5OU",
+    );
 
-		for (let i = 0; i < Object.keys(formData).length; i++) {
-			base.searchParams.append(
-				`field${i + 1}`,
-				`${Object.values(formData)[i]}`,
-			);
-		}
+    for (let i = 0; i < Object.keys(formData).length; i++) {
+      base.searchParams.append(
+        `field${i + 1}`,
+        `${Object.values(formData)[i]}`,
+      );
+    }
 
-		await fetch(base.href)
-			.then((res) => res.json())
-			.then((data) => console.log(data));
+    await fetch(base.href)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
 
-		navigate("/");
-	};
+    navigate("/");
+  };
 
-	return (
-		<div className="formSettings">
-			<form>
-				<div className="icon">
-					<Link to="/" id="X">
-						<FontAwesomeIcon icon={faXmark} />
-					</Link>
-				</div>
-				{Fields.map((field) => {
-					/* Maps over all of the fields possible to make a form */
-					const value = formData[field.id as keyof formtype];
-					return (
-						<div key={field.id}>
-							<label htmlFor={field.id}>{field.label}</label>
-							<br />
-							<input
-								id={field.id}
-								type={field.type}
-								onChange={handleChange}
-								value={value}
-								name={field.id}
-								required
-							/>
-							<br />
-						</div>
-					);
-				})}
-				<button type="submit" className="submit" onClick={updateSettings}>
-					<FontAwesomeIcon icon={faCheck} />
-				</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="formSettings">
+      <form>
+        <div className="icon">
+          <Link to="/" id="X">
+            <FontAwesomeIcon icon={faXmark} />
+          </Link>
+        </div>
+        {Fields.map((field) => {
+          /* Maps over all of the fields possible to make a form */
+          const value = formData[field.id as keyof formtype];
+          return (
+            <div key={field.id}>
+              <label htmlFor={field.id}>{field.label}</label>
+              <br />
+              <input
+                id={field.id}
+                type={field.type}
+                onChange={handleChange}
+                value={value}
+                name={field.id}
+                required
+              />
+              <br />
+            </div>
+          );
+        })}
+        <button type="submit" className="submit" onClick={updateSettings}>
+          <FontAwesomeIcon icon={faCheck} />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Settings;
