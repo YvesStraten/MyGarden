78408c900d96dd5f201e2d0dec79c061721cbd8c
YvesStraten <yves.straten@gmail.com>
2025-04-15 09:44:54 +0800

    Remove secrets, regenerate API keys

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,26 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dotenvy_macro::dotenv;
+use dynfmt::{Format, SimpleCurlyFormat};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use tauri::State;
 use tauri_plugin_http::reqwest::Client;
 use std::str::FromStr;
+
+static CHANNEL_FORMAT: &str = "https://api.thingspeak.com/channels/{}/feeds.json?api_key={}&results=1";
+static GRAPH_URL: &str = "https://thingspeak.com/channels/{}/charts/{}?bgcolor=%23ffffff&color=%23d62020&results=60&type=line&api_key={}";
+
+pub struct Ctx {
+    http: Client, 
+}
+
+impl Ctx {
+    pub fn new(http: Client) -> Self {
+        Self { http }
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 struct Data {
@@ -22,8 +37,11 @@ struct Setting {
 }
 
 #[tauri::command]
-async fn getsettings(client: State<'_, Client>) -> Result<Vec<Setting>, ()> {
-    let request = client.get("https://api.thingspeak.com/channels/2404379/feeds.json?api_key=NEFL69N7VTI5YCX1&results=1");
+async fn getsettings(ctx: State<'_, Ctx>) -> Result<Vec<Setting>, ()> {
+    let api_key = dotenv!("SETTINGS_KEY");
+    let channel = dotenv!("SETTINGS_CHANNEL");
+    let url = SimpleCurlyFormat.format(CHANNEL_FORMAT, [channel, api_key]).unwrap();
+    let request = ctx.http.get(url.as_ref());
 
     let mut parsed: Vec<Setting> = vec![];
     if let Ok(response) = request.send().await {
@@ -52,8 +70,11 @@ async fn getsettings(client: State<'_, Client>) -> Result<Vec<Setting>, ()> {
 }
 
 #[tauri::command]
-async fn fetchplots(client: State<'_,  Client>) -> Result<Vec<Data>, ()> {
-    let request = client.get("https://api.thingspeak.com/channels/2400298/feeds.json?api_key=SVWPDJG7Y4WJZUKX&results=1");
+async fn fetchplots(ctx: State<'_,  Ctx>) -> Result<Vec<Data>, ()> {
+    let api_key = dotenv!("PLOTS_KEY");
+    let channel = dotenv!("PLOTS_CHANNEL");
+    let url = SimpleCurlyFormat.format(CHANNEL_FORMAT, [channel, api_key]).unwrap();
+    let request = ctx.http.get(url.as_ref());
 
     let mut parsed: Vec<Data> = vec![];
     if let Ok(response) = request.send().await {
@@ -62,18 +83,19 @@ async fn fetchplots(client: State<'_,  Client>) -> Result<Vec<Data>, ()> {
 
         let length = &json["feeds"][0].as_object().unwrap().keys().len() - 1;
 
-        for i in 1..length {
+        for graph_number in 1..length {
             let channel_data = &json["channel"];
-            let current_field = format!("field{}", i);
+            let current_field = format!("field{}", graph_number);
 
             let value_option = json["feeds"][0].get(&current_field).unwrap();
+            let graph = SimpleCurlyFormat.format(GRAPH_URL, [channel, &graph_number.to_string(), api_key]).unwrap();
 
             let data = Data {
-            id: uuid::Uuid::new_v4(),
-            name: channel_data.get(&current_field).unwrap().clone(),
-            value: f64::from_str(value_option.as_str().unwrap()).unwrap(),
-            graph: format!("https://thingspeak.com/channels/2400298/charts/{}?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&api_key=SVWPDJG7Y4WJZUKX", i),
-        };
+                id: uuid::Uuid::new_v4(),
+                name: channel_data.get(&current_field).unwrap().clone(),
+                value: f64::from_str(value_option.as_str().unwrap()).unwrap(),
+                graph: graph.to_string()
+            };
 
             parsed.push(data);
         }
@@ -83,12 +105,13 @@ async fn fetchplots(client: State<'_,  Client>) -> Result<Vec<Data>, ()> {
 }
 
 fn main() {
-    let client = Client::new();
+    let http = Client::new();
+    let ctx = Ctx::new(http);
 
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_shell::init())
-        .manage(client)
+        .manage(ctx)
         .invoke_handler(tauri::generate_handler![fetchplots, getsettings])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
